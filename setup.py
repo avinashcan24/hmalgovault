13d2271737fa165d6a0fd4c22c7491334b38814d
avinashcan24 <avinash.can24@gmail.com>
2025-07-07 10:33:25 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='audience_metrics',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    description='Simple metrics for audience data',
+    license='MIT',
+    keywords='databricks analytics metrics',
+)
